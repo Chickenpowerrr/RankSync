d540ded6e4977e8d953a0aec0e3d3e916e288e47
Chickenpowerrr <chickenpowerrr@gmail.com>
2018-10-05 20:50:32 +0200

    remove useless comments

--- core/src/main/java/com/gmail/chickenpowerrr/ranksync/core/Main.java
+++ core/src/main/java/com/gmail/chickenpowerrr/ranksync/core/Main.java
@@ -1,31 +1,13 @@
 package com.gmail.chickenpowerrr.ranksync.core;
 
-import com.gmail.chickenpowerrr.ranksync.api.Bot;
 import com.gmail.chickenpowerrr.ranksync.api.RankSyncApi;
 import com.gmail.chickenpowerrr.ranksync.discord.BotFactory;
 
 public class Main {
 
-    private Bot discord;
-
     private Main() {
         RankSyncApiImpl.start();
         RankSyncApi.getApi().addBotFactory("Discord", BotFactory.getInstance());
-
-        /*
-        RankSyncApi.getApi().getBotFactory("Discord").getBot(new BasicProperties()
-                .addProperty("name", "SuperKoeleBot")
-                .addProperty("token", "NDkzODA0MDgzMTExNjU3NDcz.DoqSmw.6N-54Ufy6ZA583RznOOEZ0OmLUs")
-                .addProperty("guild_id", 493791190844571658L)
-                .addProperty("type", "SQL")
-                .addProperty("max_pool_size", 10)
-                .addProperty("host", "127.0.0.1")
-                .addProperty("port", 3306)
-                .addProperty("database", "ranksync")
-                .addProperty("username", "root")
-                .addProperty("password", "walrus")
-                .addProperty("rank_resource", null));
-                */
     }
 
     public static void main(String[] args) {
